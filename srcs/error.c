ce90fa1727396a5ab3bbafd22053b0c2321e5839
Rraji Abdelbari <rajiabdou9@gmail.com>
2021-04-06 16:22:32 +0100

    norm-v3 error.c

--- srcs/error.c
+++ srcs/error.c
@@ -6,13 +6,13 @@
 /*   By: arraji <arraji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/18 16:04:52 by arraji            #+#    #+#             */
-/*   Updated: 2021/04/05 14:58:12 by arraji           ###   ########.fr       */
+/*   Updated: 2021/04/06 16:22:06 by arraji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		freak_out(int bits, char *line, int index)
+int	freak_out(int bits, char *line, int index)
 {
 	if (and_op(bits, BPIPE))
 		return (error(E_SYNTAX, 1, &line[index]));
@@ -21,7 +21,7 @@ int		freak_out(int bits, char *line, int index)
 	else if (and_op(bits, BS_Q))
 		return (error(E_SYNTAX, 1, &line[index]));
 	else if (and_op(bits, BRED_TO) || and_op(bits, BRED_TO_APP)
-	|| and_op(bits, BRED_FROM))
+		|| and_op(bits, BRED_FROM))
 		return (error(E_SYNTAX, 1, &line[index]));
 	return (TRUE);
 }
@@ -36,7 +36,35 @@ void	chill(int *bits)
 	bit_off(bits, BRED_TO_APP);
 }
 
-int		error(int err, int exit_value, char *need)
+static	void	print_error(int err, char *need)
+{
+	if (err == E_SYNTAX)
+		ft_fprintf(2, "parse error near `%s'\n", need);
+	if (err == E_STANDARD)
+		ft_fprintf(2, "%s.\n", strerror(errno));
+	if (err == E_FILE)
+		ft_fprintf(2, "`%s` %s\n", need, strerror(errno));
+	if (err == E_NOCMD)
+		ft_fprintf(2, "command not found: <%s>\n", need);
+	if (err == E_WPATH)
+		ft_fprintf(2, "no such file or directory: %s\n", need);
+	if (err == E_ISDIR)
+		ft_fprintf(2, "%s: Is a directory\n", need);
+	if (err == E_ARGS)
+		ft_fprintf(2, "%s: too many arguments\n", need);
+	if (err == E_CD)
+		ft_fprintf(2, "cd: %s: no such file or directory\n", need);
+	if (err == E_NOT_VAL)
+		ft_fprintf(2, "export: `%s\': not a valid identifier\n", need);
+	if (err == E_EXIT_ARG)
+		ft_fprintf(2, "exit: `%s\' is notnumeric argument\n", need);
+	if (err == E_CD_HOME)
+		ft_fprintf(2, "cd: HOME not set\n");
+	if (err == E_NOTERM)
+		ft_fprintf(2, "term not set\n", NULL);
+}
+
+int	error(int err, int exit_value, char *need)
 {
 	if (need && need[0] == '\0' && err == E_SYNTAX)
 		need = "newline";
@@ -46,21 +74,7 @@ int		error(int err, int exit_value, char *need)
 		need[1] = '\0';
 	}
 	ft_fprintf(2, "minishell %d: ", err);
-	err == E_SYNTAX ? ft_fprintf(2, "parse error near `%s'\n", need) : 1;
-	err == E_STANDARD ? ft_fprintf(2, "%s.\n", strerror(errno)) : 1;
-	err == E_FILE ? ft_fprintf(2, "`%s` %s\n", need, strerror(errno)) : 1;
-	err == E_NOCMD ? ft_fprintf(2, "command not found: <%s>\n", need) : 1;
-	err == E_WPATH ? ft_fprintf(2, "no such file or directory: %s\n", need) : 1;
-	err == E_ISDIR ? ft_fprintf(2, "%s: Is a directory\n", need) : 1;
-	err == E_ARGS ? ft_fprintf(2, "%s: too many arguments\n", need) : 1;
-	err == E_CD ?
-	ft_fprintf(2, "cd: %s: no such file or directory\n", need) : 1;
-	err == E_NOT_VAL ?
-	ft_fprintf(2, "export: `%s\': not a valid identifier\n", need) : 1;
-	err == E_EXIT_ARG ?
-	ft_fprintf(2, "exit: `%s\' is notnumeric argument\n", need) : 1;
-	err == E_CD_HOME ? ft_fprintf(2, "cd: HOME not set\n") : 1;
-	err == E_NOTERM ? ft_fprintf(2, "term not set\n", NULL) : 1;
+	print_error(err, need);
 	g_all->exit_status = exit_value;
 	return (1);
 }
