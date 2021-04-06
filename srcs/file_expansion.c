e24dbead8379452c5d2ab883aa5b12e6fc4bbe28
Rraji Abdelbari <rajiabdou9@gmail.com>
2021-04-06 15:08:27 +0100

    norm-v3 file_expansion.c

--- srcs/file_expansion.c
+++ srcs/file_expansion.c
@@ -6,13 +6,13 @@
 /*   By: arraji <arraji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 12:05:57 by arraji            #+#    #+#             */
-/*   Updated: 2021/03/07 18:11:18 by arraji           ###   ########.fr       */
+/*   Updated: 2021/04/06 15:07:48 by arraji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void			add_file(t_command *current, char *line, int *index)
+void	add_file(t_command *current, char *line, int *index)
 {
 	t_files	*new;
 
@@ -31,7 +31,7 @@ static	void	overwrite_file(t_command *command)
 	}
 }
 
-char			*get_next_word(char *line, int *index)
+char	*get_next_word(char *line, int *index)
 {
 	int		save;
 	char	*word;
@@ -41,12 +41,11 @@ char			*get_next_word(char *line, int *index)
 	save = *index;
 	while (line[*index + 1] > 0 && line[*index + 1])
 		(*index)++;
-	if (!(word = ft_substr(line, save, (*index) - save + 1)))
-		error(E_STANDARD, 1, NULL);
+	word = ft_substr(line, save, (*index) - save + 1);
 	return (word);
 }
 
-t_bool			parse_files(t_command *current)
+t_bool	parse_files(t_command *current)
 {
 	char	type;
 	t_files	*iterator;
@@ -59,13 +58,13 @@ t_bool			parse_files(t_command *current)
 		current->file = iterator->file;
 		bit_on((int *)&(current->read_type), -1 * type);
 		if (type == RED_TO)
-			current->fd =
-			open(iterator->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			current->fd = open(iterator->file,
+					O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		else if (type == RED_FROM)
 			current->fd = open(iterator->file, O_RDONLY);
 		else if (type == RED_TO_APP)
-			current->fd =
-			open(iterator->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+			current->fd = open(iterator->file,
+					O_WRONLY | O_CREAT | O_APPEND, 0644);
 		if (current->fd < 0)
 			return (error(E_FILE, 1, current->file));
 		iterator = iterator->next;
